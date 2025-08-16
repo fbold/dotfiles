c1f186fdb79fb8e8b0a62a6988243ab9d3fee202
fbold <fbold.dev@gmail.com>
2025-08-17 01:09:33 +0200

    added fonts folder and symlinking of it to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@
 # keyd symlinking
 sudo ln -sf "$HOME/dotfiles/keyd/default.conf" /etc/keyd/default.conf
 sudo systemctl enable --now keyd
+
+# copy fonts over to user font directory
+sudo ln -sf "$HOME/dotfiles/fonts/NectoMono-Regular.ttf" "$HOME/.local/share/fonts/NectoMono-Regular.ttf"
